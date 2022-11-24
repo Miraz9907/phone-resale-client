94c864f811b22f19a1b823682dcb563c0909758c
Miraz9907 <meraz.rahman99@gmail.com>
2022-11-24 21:55:23 +0600

    categories added in home

--- src/Pages/AllCategories/Categories/Categories.js
+++ src/Pages/AllCategories/Categories/Categories.js
@@ -1,10 +1,15 @@
 import React from 'react';
 
 const Categories = ({allcategory}) => {
-    console.log(allcategory);
+    // console.log(allcategory);
     const {picture,title} = allcategory
     return (
-        <div></div>
+        <div className="card card-compact w-96 bg-base-100 shadow-xl">
+  <figure><img className='' src={picture} alt="Shoes" /></figure>
+  <div className="card-body">
+    <h2 className="card-title">{title}</h2>
+  </div>
+</div>
     );
 };
 
